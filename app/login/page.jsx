06a58c6c91bcc4e0858b848f6a6fdafe29e94fa2
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-09-26 12:54:34 -0500

    Improved Authentication with custom validation with the help of yup and useForm

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,36 +3,55 @@
 import { useState } from "react";
 import { auth } from "../../firebase"; // Import the googleProvider
 import { signInWithEmailAndPassword } from "firebase/auth";
-
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import GoogleBtn from "../components/auth-utils/GoogleBtn";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const router = useRouter();
 
+  // Validation schema
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
   // Handle email/password login
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (data) => {
+    const { email, password } = data;
     try {
       await signInWithEmailAndPassword(auth, email, password);
       router.push("/"); // Redirect to homepage after login
     } catch (err) {
-      setError(err.message);
+      if (err.code === "auth/wrong-password") {
+        setError("Invalid password.");
+      } else if (err.code === "auth/user-not-found") {
+        setError("User not found.");
+      } else {
+        setError("An error occurred. Please try again.");
+      }
     }
   };
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-50">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-lg rounded-md">
-        <h2 className="text-2xl font-bold text-gray-900 text-center">Login</h2>
+      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-md">
         {error && <p className="text-red-500 text-center">{error}</p>}
 
         {/* Email/Password Login Form */}
-        <form onSubmit={handleLogin} className="space-y-6">
+        <form onSubmit={handleSubmit(handleLogin)} className="space-y-6">
           <div>
             <label
               htmlFor="email"
@@ -43,10 +62,14 @@ const LoginPage = () => {
             <input
               type="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              {...register("email")} // Register input for validation
+              className={`block w-full px-3 py-2 bg-gray-50 border ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500`}
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
           </div>
           <div>
             <label
@@ -58,10 +81,14 @@ const LoginPage = () => {
             <input
               type="password"
               id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              {...register("password")} // Register input for validation
+              className={`block w-full px-3 py-2 bg-gray-50 border ${
+                errors.password ? "border-red-500" : "border-gray-300"
+              } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500`}
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
           </div>
           <button
             type="submit"
@@ -73,7 +100,7 @@ const LoginPage = () => {
         <GoogleBtn setError={setError} />
 
         <p className="text-center text-gray-600">
-          Don't have an account?{" "}
+          {"Don't have an account?"}{" "}
           <Link
             href="/signup"
             className="text-indigo-600 hover:text-indigo-500"
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -4,32 +4,52 @@ import { useState } from "react";
 import { auth } from "../../firebase"; // Import the googleProvider
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import GoogleBtn from "../components/auth-utils/GoogleBtn";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const router = useRouter();
-
-  const handleSignup = async (e) => {
-    e.preventDefault();
+  const [passwordChecker, setPasswordChecker] = useState("");
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters"),
+    confirmPassword: Yup.string()
+      .required("Please confirm your password")
+      .oneOf([Yup.ref("password")], "Passwords don't match"), // Check if passwords match
+  });
+  const onSubmit = async () => {
+    // e.preventDefault();
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       router.push("/login");
     } catch (err) {
-      setError(err.message);
+      if (err.code === "auth/email-already-in-use") {
+        setError("This email is already in use.");
+      } else {
+        setError("An error occurred. Please try again.");
+      }
     }
   };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+  const [error, setError] = useState(null);
+  const router = useRouter();
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-50">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-lg rounded-md">
-        <h2 className="text-2xl font-bold text-gray-900 text-center">
-          Sign Up
-        </h2>
+      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-md">
         {error && <p className="text-red-500 text-center">{error}</p>}
-        <form onSubmit={handleSignup} className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div>
             <label
               htmlFor="email"
@@ -41,9 +61,13 @@ const Signup = () => {
               type="email"
               id="email"
               value={email}
+              {...register("email")}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </div>
           <div>
             <label
@@ -56,9 +80,32 @@ const Signup = () => {
               type="password"
               id="password"
               value={password}
+              {...register("password")}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm password
+            </label>
+            <input
+              type="password"
+              id="password-check"
+              value={passwordChecker}
+              {...register("confirmPassword")}
+              onChange={(e) => setPasswordChecker(e.target.value)}
+              className="block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500">{errors.confirmPassword.message}</p>
+            )}
           </div>
           <button
             type="submit"
