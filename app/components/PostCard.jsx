421ad8a5d8e362b86c8960e097fd59ec617230e2
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-02 13:21:27 -0500

    Fixed link for post

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -84,7 +84,7 @@ const PostCard = ({ post }) => {
               width={50}
               height={50}
             />
-            <Link href={`/profile/${user.uid}`} className="font-semibold ml-2">
+            <Link href={`/profile/${authorId}`} className="font-semibold ml-2">
               {postUser.displayName}
             </Link>
           </>
